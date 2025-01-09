014741acb1f27912a5eedd6e038347b404d51840
c7506663 <dolly2638@student.leedsbeckett.ac.uk>
2025-01-09 13:36:44 +0000

    exam commit

---
+++ main.py
@@ -0,0 +1,8 @@
+exam commit
+
+Enter the numbers of rows and columns of matrices: 2
+Enter the elements of first matrices: 1 2 3 4
+Enter the elements of second matrix: 1 2 3 4
+sum of enetered matrices:
+2     4
+6     8
